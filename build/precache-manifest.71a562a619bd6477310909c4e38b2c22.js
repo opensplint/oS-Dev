978088522d0cb8997f8ac8c5d48042002364863e
Sajana Vijayan <vsajana@gmail.com>
2020-08-05 17:35:19 +0400

    changes

---
+++ build/precache-manifest.71a562a619bd6477310909c4e38b2c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35813cc7607fbd6d59fd6a646a2ea2f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1dfcad84a7cf14a8545",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0313771a5b401caf2b71",
+    "url": "/static/js/2.7736d3cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7736d3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1dfcad84a7cf14a8545",
+    "url": "/static/js/main.2bd06ded.chunk.js"
+  },
+  {
+    "revision": "26f11fa7295f2d69fa32",
+    "url": "/static/js/runtime-main.83af73b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
